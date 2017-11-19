c853f9c83d6dfe2ec4fdbb1beec5873d19bf7e73
Zhanfeng Huang <vipyellowbee@gmail.com>
2017-11-19 12:29:34 -0500

    modify

--- read.py
+++ read.py
@@ -2,7 +2,7 @@ import pickle
 import json
 import os
 
-file_des = os.path.join(os.path.dirname(__file__) , '8000000_80126615.pkl')
+file_des = os.path.join(os.path.dirname(__file__) , '8000000_8012661.pkl')#change the name of the .pkl file
 output_des = os.path.join(os.path.dirname(__file__) , 'output.json')
 
 read = []
@@ -17,11 +17,3 @@ with open(file_des, "rb") as src:
 with open(output_des, 'w') as o:
     json.dump(read, o)
 o.close()
-
-#print (read[39]['@ID'])
-#print (len(read[39]['TrackDetail']))
-#for i in range(0, len(read[39]['TrackDetail'])):
-#    print (read[39]['TrackDetail'][i]['EventTime'])
-#    print (read[39]['TrackDetail'][i]['EventDate'])
-#    print (read[39]['TrackDetail'][i]['Event'])
-#    print (read[39]['TrackDetail'][i]['EventZIPCode'])
