588fae849de85853a1c6bf1933c129d93e2e0035
Zhanfeng Huang <vipyellowbee@gmail.com>
2017-11-30 00:21:06 -0500

    Convert ZIP code to Coordinate

---
+++ zipToCoordinate.py
@@ -0,0 +1,40 @@
+import os
+import json
+import pandas as pd
+
+lat = []
+lon = []
+flag = 0
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(os.path.join(__location__, 'dev-geo.json')) as src:
+    coordinate = json.load(src)
+
+count = len(coordinate)#41081
+
+data = pd.read_csv(os.path.join(__location__, 'des_ctn.csv'), dtype = str)
+data.head()
+
+zip = data['destination']
+countZip = len(zip)
+
+for i in range(0, countZip):
+    for j in range(0, count):
+        if coordinate[j]['city_zip'] == zip[i]:
+            lat.append(coordinate[j]['city_lat'])
+            lon.append(coordinate[j]['city_lon'])
+            flag = 1
+    if flag == 0:
+        lat.append('none')
+        lon.append('none')
+        flag = 1
+    flag = 0
+
+with open(os.path.join(__location__, 'output.csv'), 'w') as output:
+    output.write('lat,lon\n')
+    for i in range(0, countZip):
+        output.write(str(lat[i]))
+        output.write(',')
+        output.write(str(lon[i]))
+        output.write('\n')
